cdc6829bf167b6e3fe9a6a69a7ec2e048ee67bef
Sameerlm10 <sam.choulm10@gmail.com>
2024-01-25 20:35:35 +0530

    updated project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import DataBase.CourierStorage;
+import DataBase.DummyData;
+import Implementation.CourierImplementation;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CourierImplementation c = new CourierImplementation();
+
+        c.addPackage(DummyData.dummyData());
+        c.addPackage(DummyData.dummyData2());
+        c.addPackage(DummyData.dummyData3());
+        c.addPackage(DummyData.dummyData4());
+
+        System.out.println(CourierStorage.ourData());
+
     }
+
 }
